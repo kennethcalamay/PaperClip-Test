6d033c51659f6b120d6f6df9fa8eb2dee8a31594
Kenneth Calamay <github@k.calamay.me>
2011-09-14 13:11:10 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poc::Application.config.secret_token = '9d6747da5c88572ecf46f04455c76acd628f521eb12753bfcea905db35d51e92ad8686f9448925a2434ad15d39594fd7b4e37e9776dc25ddc55b9afa8d4635d0'
